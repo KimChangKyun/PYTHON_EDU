d8ebe7fcecdee662041c55ea45712c4592154fc1
D2D <45621009+KimChangKyun@users.noreply.github.com>
2019-05-16 20:36:00 +0900

    Create 구구단.py
    한줄 구구단.. 근데 WHILE 문은 어떻게 해야할지 모르겠음 ...

---
+++ 02/구구단.py
@@ -0,0 +1,8 @@
+import time
+
+a = 2
+list1 = [("%d*%d=%d"%(i,j,i*j)) for i in range(2,10) for j in range(1, 10)]
+list2 = [("%d*%d=%d"%(a,j,a*j)) while a<10 (a=a+1) for j in range(1, 10)]
+print(list2)
+
+time.sleep(30)  # 10초 멈추기
