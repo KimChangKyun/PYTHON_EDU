a10ec1a41775950def98afae52ebd791386b382a
D2D <45621009+KimChangKyun@users.noreply.github.com>
2019-05-21 22:11:29 +0900

    Update 구구단.py

--- 02/구구단.py
+++ 02/구구단.py
@@ -1,8 +1,54 @@
 import time
 
 a = 2
-list1 = [("%d*%d=%d"%(i,j,i*j)) for i in range(2,10) for j in range(1, 10)]
-list2 = [("%d*%d=%d"%(a,j,a*j)) while a<10 (a=a+1) for j in range(1, 10)]
-print(list2)
+b = 10
 
-time.sleep(30)  # 10초 멈추기
+class forWhile:
+	#1) for문과 for문
+	def for_for(self): 
+		list = []
+		list = [("%d*%d=%d"%(i,j,i*j)) for i in range(a,b) for j in range(a, b)]
+		return list
+	#2) for문과 while문
+	def for_while(self): 
+		list = []
+		for i in range(a,b) :
+			j = a
+			while j < b :
+				list.append(str(i) + "*" + str(j) + "=" + str(i*j))
+				j += 1
+		return list
+	#3) while문과 for문
+	def while_for(self):
+		list = []
+		i = a
+		while i < b :
+			for j in range(a,b) :
+				list.append(str(i) + "*" + str(j) + "=" + str(i*j))
+			i += 1
+		return list
+	#4) while문과 while문
+	def while_while(self):
+		list = []
+		i = a
+		while i < b :
+			j = a
+			while j<10 :
+				list.append(str(i) + "*" + str(j) + "=" + str(i*j))
+				j += 1
+			i += 1 
+		return list
+
+
+forWhile = forWhile()
+
+print("#1) for문과 for문")
+print(forWhile.for_for())
+print("#2) for문과 while문")
+print(forWhile.for_while())
+print("#3) while문과 for문")
+print(forWhile.while_for())
+print("#4) while문과 while문")
+print(forWhile.while_while())
+
+time.sleep(10) # 10초 멈추기
